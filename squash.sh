dc00ee2e1410839cbb3dfcb7c91beb8badde8968
Petr Moucha <moucha19@gmail.com>
2024-08-18 12:40:35 +0200

    Updated squash script with fn value to avoid issues when using OpenSCAD workbench in FreeCAD

--- squash.sh
+++ squash.sh
@@ -2,5 +2,5 @@
 
 sed -e '/use <components.scad>/{r ./scad/components.scad' -e 'd}' ./scad/fletching_jig.scad > ./tmp.scad
 grep -v -e '^\s*//' -e '^\s*$' -e '^\s*[[:alpha:]]*_treshold' ./tmp.scad > ./doc/squashed.scad
-echo -e "\njig(part_select=0);" >> ./doc/squashed.scad
+echo -e "\n\$fn=30;\njig(part_select=0, fn=30);" >> ./doc/squashed.scad
 rm tmp.scad
